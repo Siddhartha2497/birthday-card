f6bbf8c4770c7365683a1a5fe1dcee3156ffc39b
Siddhartha Sekhar Ghosh <908895@cognizant.com>
2025-06-03 20:20:48 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+// import path from 'path'; 
+// import sound from 'sound-play';
+// const __dirname = path.resolve();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Replace with your actual correct values
+const correctName = 'siddhartha';
+//const correctDob = '1997-09-24';
+const correctDob = '2025-06-01';
+
+// console.log(correctDob);
+// console.log(correctName);
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+// const filePath = path.join(__dirname, "tune_song.mp3");
+// sound.play(filePath);
+// API endpoint
+app.post('/validate', (req, res) => {
+const { name, dob } = req.body;
+
+// console.log('Received:',name,dob);
+if (
+name.trim().toLowerCase() === correctName &&
+dob === correctDob
+) {
+return res.json({ success: true, message: 'Correct',
+memoryLink: 'https://drive.google.com/file/d/1QjYnb-7krlmTjxELvU7OVSkEGcq9SHhb/view?usp=drive_link' // replace with real URL
+});
+} else {
+return res.json({ success: false, message: 'Wrong name or DOB' });
+}
+});
+
+// Start server
+app.listen(PORT, () => {
+console.log(`Server running on http://localhost:${PORT}`);
+});
