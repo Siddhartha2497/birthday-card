78ddbc62fd95224bf338ccaf84b9e88f73247557
Siddhartha Sekhar Ghosh <908895@cognizant.com>
2025-06-03 21:42:30 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Replace with your actual correct values
-// const correctName = 'siddhartha';
-// //const correctDob = '1997-09-24';
-// const correctDob = '2025-06-01';
 
-// console.log(correctDob);
-// console.log(correctName);
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -25,9 +19,7 @@ app.use(express.urlencoded({extended:true}));
 const correctName=process.env.CORRECT_NAME;
 const correctDob=process.env.CORRECT_DOB;
 const memoryLink=process.env.MEMORY_LINK;
-// const filePath = path.join(__dirname, "tune_song.mp3");
-// sound.play(filePath);
-// API endpoint
+
 app.post('/validate', (req, res) => {
 const { name, dob } = req.body;
 
@@ -38,7 +30,6 @@ dob === correctDob
 ) {
 return res.json({ success: true, message: 'Correct',
 memoryLink: memoryLink
-// 'https://drive.google.com/file/d/1QjYnb-7krlmTjxELvU7OVSkEGcq9SHhb/view?usp=drive_link' // replace with real URL
 });
 } else {
 return res.json({ success: false, message: 'Wrong name or DOB' });
